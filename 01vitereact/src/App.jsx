3727869263ca34ac7f850ecd2a369a04578111b0
Bijay-Prasad <bijaylife2001@gmail>
2024-04-11 20:25:12 +0530

    foundations of reactjs

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./Chai"
+
+function App() {
+  const username = 'Bijay Prasad'
+
+  return (
+    <>
+    <h1>Chai aur Code | {username}</h1>
+    <Chai/>
+    </>
+  )
+}
+
+export default App
